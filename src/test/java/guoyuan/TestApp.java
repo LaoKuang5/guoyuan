a87f1ab9f4ab1bcabcba8473e9dd6987df782802
jones <yjb235@foxmail.com>
2018-08-18 17:31:34 +0800

    测试注入

--- src/test/java/guoyuan/TestApp.java
+++ src/test/java/guoyuan/TestApp.java
@@ -6,8 +6,6 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,8 +15,8 @@ import cn.edu.glut.model.Emp;
 @ContextConfiguration("/spring-common.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TestApp {
-	@Resource
-	EmpServiceImpl es;
+	@Resource(name="empService")
+	EmpService es;
 	@Test
 	public void test() {
 		List<Emp> emps=es.getAll();
