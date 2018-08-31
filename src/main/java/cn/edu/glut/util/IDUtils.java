d400f152696961aa9ca58429637cad1d39e82140
Kuangzhenwu <2964105272@qq.com>
2018-08-31 17:02:33 +0800

    新添加： id工具类：产生商品id、和图片id 公共响应类

---
+++ src/main/java/cn/edu/glut/util/IDUtils.java
@@ -0,0 +1,44 @@
+package cn.edu.glut.util;
+
+import java.util.Random;
+
+/**
+ * 各种id生成策略
+ * @author Kuang
+ *
+ */
+public class IDUtils {
+
+	/**
+	 * 图片名生成
+	 */
+	public static String genImageName() {
+		//取当前时间的长整形值包含毫秒
+		long millis = System.currentTimeMillis();
+		//加上三位随机数
+		Random random = new Random();
+		int end3 = random.nextInt(999);
+		//如果不足三位前面补0
+		StringBuffer sb = new StringBuffer().append(millis).append(String.format("%03d", end3));
+		return sb.toString();
+	}
+	
+	/**
+	 * 商品id生成
+	 */
+	public static long genCommodityId() {
+		//取当前时间的长整形值包含毫秒
+		long millis = System.currentTimeMillis();
+		//加上两位随机数
+		Random random = new Random();
+		int end2 = random.nextInt(99);
+		StringBuffer sb = new StringBuffer().append(millis).append(String.format("%02d", end2));
+		long id = new Long(sb.toString());
+		return id;
+	}
+	
+	public static void main(String[] args) {
+		for(int i=0;i< 100;i++)
+		System.out.println(genImageName());
+	}
+}
