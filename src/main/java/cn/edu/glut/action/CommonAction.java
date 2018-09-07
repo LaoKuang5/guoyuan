2249e1abfc4ae843d5b15aa10c1602a02e4454a5
jones <yjb235@foxmail.com>
2018-09-07 11:28:39 +0800

    新增拦截器

---
+++ src/main/java/cn/edu/glut/action/CommonAction.java
@@ -0,0 +1,57 @@
+package cn.edu.glut.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 界面跳转支持 控制页面跳转基本逻辑
+ * @author jones
+ *
+ */
+@Controller
+@RequestMapping("common")
+public class CommonAction {
+	
+	@RequestMapping("home")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("video")
+	public String video() {
+		return "video";
+	}
+
+	@RequestMapping("buylist")
+	public String buylist() {
+		return "buyList";
+	}
+
+	@RequestMapping("my")
+	public ModelAndView my(HttpSession session) {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("my");
+		mv.addObject("user", session.getAttribute("user"));
+		return mv;
+	}
+	
+	@RequestMapping("message")
+	public String message() {
+		return "message";
+	}
+	
+	@RequestMapping("fruitSupermarket")
+	public String fruitSupermarket() {
+		return "fruitSupermarket";
+	}
+	
+	@RequestMapping("login")
+	public String login() {
+		System.out.println("轻轻地我来过");
+		return "login";
+	}
+}
---
+++ src/main/java/cn/edu/glut/interceptor/LoginCheck.java
@@ -0,0 +1,48 @@
+package cn.edu.glut.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.edu.glut.model.UserInfo;
+
+/**
+ * 拦截器类 用作请求之前判断是否已经登录
+ * @author 于金彪
+ *
+ */
+public class LoginCheck implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("拦截器:"+request.getPathInfo());
+		UserInfo user=(UserInfo) request.getSession().getAttribute("user");
+		if(user==null) {
+			//未登录 返回登录页面
+			System.out.println();
+			request.getRequestDispatcher("login.action").forward(request, response);
+			return false;
+		}else {
+			return true;
+		}
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
